c89462b3bcb813b9bca8504d2bf18955852f14a1
Misaki Koonce <misaki.koonce@gmail.com>
2018-12-06 19:57:50 -0800

    add timer

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
-//All card items
-
-// * Add shuffle function
-// When should it work? - the very first screen and when the player click the refresh button.
-
-
-
 class MemoryGame {
-    constructor() {
+    constructor(timer) {
+        this.timer = timer;
+        this.timer.reset();
+
         this.flippedCard = false;
         this.lockBoard = false;
         this.firstCard = true;
         this.secondCard = true;
+    
         this.initCards();
     }
 
@@ -19,7 +16,7 @@ class MemoryGame {
         let flipped = document.querySelectorAll('.flip');
 
         // unflip every cards
-        flipped.forEach(function(card) {
+        flipped.forEach((card) => {
             card.classList.remove('flip');
         });
 
@@ -29,14 +26,16 @@ class MemoryGame {
             setTimeout (() => this.initCountries(), 700);
         }
     }
+    
 
     initCountries() {
         // prepare 16 shuffled countries for the 16 cards
         let countries = ['australia','australia','egypt','egypt','france','france','india','india','italy','italy','japan','japan','london','london','usa','usa'];
-        countries = countries.sort(function(a, b){return 0.5 - Math.random()});
+        // got an idea from here: https://www.w3schools.com/js/js_array_sort.asp
+        countries = countries.sort((a, b) => {return 0.5 - Math.random()});
 
         // for each .card element
-        document.querySelectorAll('.card').forEach(function(card, i) {
+        document.querySelectorAll('.card').forEach((card, i) => {
 
             // grab the country from countries using the index
             let country = countries[i];
@@ -53,9 +52,13 @@ class MemoryGame {
         if (this.lockBoard) return;
         // current clicked card should equal to first card so player cannot click the same card       
         if (card === this.firstCard) return;
-    
+        
+        if (!this.timer.started) {
+            this.timer.start();
+        }
+
         card.classList.add('flip');
-    
+        
         if (!this.flippedCard) {
             //first card click
             this.flippedCard = true;
@@ -71,6 +74,7 @@ class MemoryGame {
         }
     } 
 
+
     checkForMatch() {
         //if they match stay facing up
         if (this.firstCard.dataset.country === this.secondCard.dataset.country) {
@@ -81,14 +85,16 @@ class MemoryGame {
         } else {
         // if they don't match flip back
         this.lockBoard = true; 
+        // Set timeout to prevent they remove 'flip' immediately.
             setTimeout(() => {
                 this.firstCard.classList.remove('flip');
                 this.secondCard.classList.remove('flip');
-                this.resetBoard();
+                this.endTurn();
         }, 800)};
     }
 
-    resetBoard() {
+    // First card and second card variables should be reset after each turn.
+    endTurn() {
         [this.flippedCard, this.lockBoard] = [false, false];
         [this.firstCard, this.secondCard] = [null, null];
     }
@@ -96,25 +102,91 @@ class MemoryGame {
     
 };
 
+class Timer {
+    constructor(element){
+        this.element = element;
 
-let game = new MemoryGame();
+        this.seconds = 0;
+        this.minutes = 0;
+        this.interval = null;
+        this.started = false;
+    }
+
+    start() {   
+        this.started = true;
+        this.interval = setInterval( () => {
+            ++this.seconds;
+
+            if(this.seconds === 60) {
+                this.seconds = 0;
+                this.minutes++;
+            }
+            
+            this.element.innerHTML = this.toString();
+
+        },1000);
+    }
+
+    stop() {
+        this.started = false;
+        clearInterval(this.interval);
+    }
+
+    reset() {
+        this.stop();
+        this.seconds = 0;
+        this.minutes = 0;
+        this.element.innerHTML = this.toString();
+    }
+
+    toString() {
+        let displaySeconds = 0;
+        let displayMinutes = 0;
+
+        //minutes and seconds always have two digit
+        if(this.seconds < 10) {
+            displaySeconds = `0${this.seconds.toString()}`;
+        } else {
+            displaySeconds = this.seconds;
+        }
+
+        if (this.minutes < 10) {
+            displayMinutes = `0${this.minutes.toString()}`;
+        } else {
+            displayMinutes = this.minutes;
+        }
+
+        return `${displayMinutes}:${displaySeconds}`;
+    }
+
+   
+}
+
+let timer = new Timer(document.querySelector('.timer'));
+let game = new MemoryGame(timer);
+
 
 let cards = document.querySelectorAll('.card');
-cards.forEach(card => card.addEventListener('click',function() {
+cards.forEach(card => card.addEventListener('click', function() {
     game.flipCard(this);
 }));
 
-document.querySelector('#reset').addEventListener('click', function () {
-    game = new MemoryGame();
+/*
+ Need to choose first card
+.addEventListener('click', function() {
+    activateTimer = new Timer();
+});
+*/
+
+
+document.querySelector('#reset').addEventListener('click', () => {
+    game = new MemoryGame(timer);
 });
 
 
 
-// * Add a counting timer
-// the timer starts from 00.00
-// When the player click the first card, the timer starts
 // Once the player win (the last pair matched), the timer stops.
-// when the player hit refresh button, the timer back to 0.
+
 
 
 // * Add star rating
@@ -124,8 +196,9 @@ document.querySelector('#reset').addEventListener('click', function () {
 
 
 // * Add a move counter next to the star rating
-//  When the player click the card twice (one pair of move), the counter count up.
-
+//  When the player click the card twice (one pair of move), the counter counts up.
+// When the player refresh the page, the counter back to 0.
 
 
 // * Once every cards face up, the player wins and congrats screen shows up
+// 
